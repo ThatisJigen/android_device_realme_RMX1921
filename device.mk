bb3976974450e58f7f5ba8c8e91ffbc16f7fa93c
pjgowtham <pjgowtham@gmail.com>
2020-09-19 02:25:07 +0530

    RMX1921: Add input group to sensors service
    * Replace uhid with input
    * service as is from hardware/interfaces
    * Binderize sensor hal

--- device.mk
+++ device.mk
@@ -375,7 +375,7 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl.xt \
-    android.hardware.sensors@1.0-service \
+    android.hardware.sensors@1.0-service.xt \
     libsensorndkbridge
 
 # Soong namespaces
